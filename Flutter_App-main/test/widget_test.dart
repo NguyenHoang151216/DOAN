f99e47790531eb515ee4e35afd5ae71faf3d16ef
NguyenHoang151216 <hoangarmy4@gmail.com>
2026-05-28 17:09:59 +0700

    Update

--- Flutter_App-main/test/widget_test.dart
+++ Flutter_App-main/test/widget_test.dart
@@ -7,13 +7,16 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 import 'package:flutter_app/main.dart';
 
 void main() {
   testWidgets('App renders initial screen', (WidgetTester tester) async {
+    SharedPreferences.setMockInitialValues({});
+
     await tester.pumpWidget(const WhiteHotelApp());
-    await tester.pumpAndSettle();
+    await tester.pump();
 
     expect(find.byType(MaterialApp), findsOneWidget);
   });
